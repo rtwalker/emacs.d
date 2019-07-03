bd5eb3294138e94a4edf1621390c506bf9beaf82
Ryan Walker <walker.ryan.tate@gmail.com>
2019-07-03 17:04:14 -0400

    evil-collection: mute for now--will add in pieces as needed

--- init.el
+++ init.el
@@ -365,13 +365,6 @@ Prefix arg VIS toggles visibility of ess-code as for `ess-eval-region'."
   (evil-insert-state-message nil)
   :config (evil-mode 1))
 
-(use-package evil-collection
-  :after evil
-  :init
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-collection-init))
-
 (use-package evil-ledger
   :after ledger-mode
   :custom
