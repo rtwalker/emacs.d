075ba69cd96d455315ecd8a47277bacc101f8e7e
Ryan Walker <rtwalker@hey.com>
2023-02-09 12:42:23 -0500

    modify display-buffer-alist

--- rtwalker.el
+++ rtwalker.el
@@ -13,6 +13,27 @@
   :after evil
   :config (global-evil-surround-mode 1))
 
+(use-package window
+  :config
+  (setq display-buffer-alist
+        `(("^\\*compilation"
+           (display-buffer-in-side-window)
+           (side . bottom)
+           (window-height . 40)
+           (reusable-frames . nil))
+          ("^magit: "
+           (display-buffer-reuse-window display-buffer-in-side-window)
+           (side . left)
+           (slot . 1)
+           (window-width . 0.33)
+           (reusable-frames . nil))
+          ("^\\*Help"
+           (display-buffer-reuse-window display-buffer-in-side-window)
+           (side . right)
+           (slot . 1)
+           (window-width . 0.33)
+           (reusable-frames . nil)))))
+
 (use-package which-key
   :custom
   (which-key-allow-evil-operators 1)
