840a70ee10ec85d85f42aa001ad34ef2d6e420ec
Ryan Walker <rtwalker@hey.com>
2024-07-24 13:41:50 -0400

    Assimilate popper v0.4.7-8-g66b4769

--- rtwalker.el
+++ rtwalker.el
@@ -269,6 +269,20 @@
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
+(use-package popper
+  :bind (("C-`"   . popper-toggle)
+         ("M-`"   . popper-cycle)
+         ("C-M-`" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$"
+          "\\*Async Shell Command\\*"
+          help-mode
+          compilation-mode))
+  (popper-mode +1)
+  (popper-echo-mode +1)) ; For echo area hints
+
 (use-package racket-mode
   :defer t)
 
