1b40bc218700e39bcdd578930f8312d5876dd257
Jonas Bernoulli <jonas@bernoul.li>
2024-08-24 19:54:54 +0200

    Emacsg: Clean up early load-path additions
    `auto-compile' no longer depends on `packed' and `compat'.

--- early-init.el
+++ early-init.el
@@ -8,10 +8,9 @@
 (setq load-prefer-newer t)
 
 (let ((dir (file-name-directory (or load-file-name buffer-file-name))))
-  (add-to-list 'load-path (expand-file-name "lib/compat" dir))
-  (add-to-list 'load-path (expand-file-name "lib/packed" dir))
   (add-to-list 'load-path (expand-file-name "lib/auto-compile" dir))
   (add-to-list 'load-path (expand-file-name "lib/gcmh" dir)))
+
 (require 'auto-compile)
 (auto-compile-on-load-mode)
 (auto-compile-on-save-mode)
