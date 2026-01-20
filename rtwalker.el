9073e249904cc493940ad57b8a6b2c7305d35a00
Ryan Walker <rtwalker@hey.com>
2026-01-20 14:28:35 -0500

    corfu: preselect first candidate

--- rtwalker.el
+++ rtwalker.el
@@ -62,17 +62,10 @@
   (setq xref-show-xrefs-function #'consult-xref))
 
 (use-package corfu
-  ;; Optional customizations
   :custom
-  (corfu-cycle t)                   ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                    ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-preselect 'first)
   :init
   (global-corfu-mode))
 
