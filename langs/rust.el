bda4595a8c376e50424d2ded0bc44b561d74af8a
Ryan Walker <rtwalker@hey.com>
2026-02-25 09:03:23 -0500

    rust-mode: move definitions into use-package declaration

--- langs/rust.el
+++ langs/rust.el
@@ -5,66 +5,69 @@
 
 ;;; Code:
 
-(defface rust-unsafe-keyword-face
-  `((t :family ,(if-let* ((font "PragmataPro Mono Liga Fraktur")
-                          ((find-font (font-spec :name font))))
-                    font
-                  (face-attribute 'default :family))
-       :weight bold
-       :slant italic
-       :inherit warning))
-  "Face for the `unsafe` keyword."
-  :group 'rust)
-
-(defface rust-unsafe-block-face
-  `((t :background ,(modus-themes-get-color-value 'bg-red-nuanced) :extend t :foreground unspecified))
-  "Face for `unsafe` block content."
-  :group 'rust)
-
-(defun rtw/rust-ts-font-lock ()
-  (when (treesit-ready-p 'rust)
-    ;; make "unsafe-block" a "level 3" feature
-    (setq-local treesit-font-lock-feature-list
-                '(( comment definition)
-                  ( keyword string)
-                  ( assignment attribute builtin constant escape-sequence
-                    number type unsafe-block )
-                  ( bracket delimiter error function operator property variable)))
-    (let ((unsafe-rules
-           (treesit-font-lock-rules
-            :language 'rust :feature 'keyword :override t '(("unsafe" @rust-unsafe-keyword-face))
-            :language 'rust :feature 'unsafe-block :override 'keep "(unsafe_block) @rust-unsafe-block-face")))
-      (setq-local treesit-font-lock-settings
-                  (append unsafe-rules treesit-font-lock-settings))
-      (treesit-font-lock-recompute-features))))
-
-(defun rtw/rust-ts-imenu ()
-  (when (treesit-ready-p 'rust)
-    (setq-local treesit-simple-imenu-settings
-                `(("Module" "\\`mod_item\\'" nil nil)
-                  ("Enum" "\\`enum_item\\'" nil nil)
-                  ("Impl Trait"
-                   "\\`impl_item\\'"
-                   (lambda (node)
-                     (treesit-node-child-by-field-name node "trait"))
-                   nil)
-                  ("Impl"
-                   "\\`impl_item\\'"
-                   (lambda (node)
-                     (not (treesit-node-child-by-field-name node "trait")))
-                   nil)
-                  ("Type" "\\`type_item\\'" nil nil)
-                  ("Struct" "\\`struct_item\\'" nil nil)
-                  ("Fn" "\\`function_item\\'" nil nil)
-                  ("Macro" "\\`macro_definition\\'" nil nil)
-                  ("Trait" "\\`trait_item\\'" nil nil)
-                  ("Union" "\\`union_item\\'" nil nil)))))
-
 (use-package rust-ts-mode
   :config
   (setq rust-ts-mode-prettify-symbols-alist nil)
+
+  (defface rust-unsafe-keyword-face
+    `((t :family ,(if-let* ((font "PragmataPro Mono Liga Fraktur")
+                            ((find-font (font-spec :name font))))
+                      font
+                    (face-attribute 'default :family))
+         :weight bold
+         :slant italic
+         :inherit warning))
+    "Face for the `unsafe` keyword."
+    :group 'rust)
+
+  (defface rust-unsafe-block-face
+    `((t :background ,(modus-themes-get-color-value 'bg-red-nuanced) :extend t :foreground unspecified))
+    "Face for `unsafe` block content."
+    :group 'rust)
+
+  (defun rtw/rust-ts-font-lock ()
+    (when (treesit-ready-p 'rust)
+      ;; make "unsafe-block" a "level 3" feature
+      (setq-local treesit-font-lock-feature-list
+                  '(( comment definition)
+                    ( keyword string)
+                    ( assignment attribute builtin constant escape-sequence
+                      number type unsafe-block )
+                    ( bracket delimiter error function operator property variable)))
+      (let ((unsafe-rules
+             (treesit-font-lock-rules
+              :language 'rust :feature 'keyword :override t '(("unsafe" @rust-unsafe-keyword-face))
+              :language 'rust :feature 'unsafe-block :override 'keep "(unsafe_block) @rust-unsafe-block-face")))
+        (setq-local treesit-font-lock-settings
+                    (append unsafe-rules treesit-font-lock-settings))
+        (treesit-font-lock-recompute-features))))
+
   (add-hook 'rust-ts-mode-hook #'rtw/rust-ts-font-lock)
+
+  (defun rtw/rust-ts-imenu ()
+    (when (treesit-ready-p 'rust)
+      (setq-local treesit-simple-imenu-settings
+                  `(("Module" "\\`mod_item\\'" nil nil)
+                    ("Enum" "\\`enum_item\\'" nil nil)
+                    ("Impl Trait"
+                     "\\`impl_item\\'"
+                     (lambda (node)
+                       (treesit-node-child-by-field-name node "trait"))
+                     nil)
+                    ("Impl"
+                     "\\`impl_item\\'"
+                     (lambda (node)
+                       (not (treesit-node-child-by-field-name node "trait")))
+                     nil)
+                    ("Type" "\\`type_item\\'" nil nil)
+                    ("Struct" "\\`struct_item\\'" nil nil)
+                    ("Fn" "\\`function_item\\'" nil nil)
+                    ("Macro" "\\`macro_definition\\'" nil nil)
+                    ("Trait" "\\`trait_item\\'" nil nil)
+                    ("Union" "\\`union_item\\'" nil nil)))))
+
   (add-hook 'rust-ts-mode-hook #'rtw/rust-ts-imenu)
+
   (add-to-list 'treesit-language-source-alist '(rust . ("https://github.com/tree-sitter/tree-sitter-rust.git"))))
 
 (use-package rust-mode
