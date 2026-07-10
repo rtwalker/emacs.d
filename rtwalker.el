0db5b3f0ba8d0e398c382042be5444e991ed3371
Ryan Walker <rtwalker@hey.com>
2026-07-10 12:09:39 -0400

    nerd-icons-dired: Drop custom icon functions
    Something caused these to break, and I'd rather just use the default
    than track down a fix.

--- rtwalker.el
+++ rtwalker.el
@@ -336,14 +336,9 @@
   (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
 
 (use-package nerd-icons-dired
-  :preface
-  (defun rtw/nerd-icons-icon-for-file (file)
-    (nerd-icons-icon-for-file file :height 1.25))
-  (defun rtw/nerd-icons-icon-for-dir (dir)
-    (nerd-icons-icon-for-dir dir :height 1.25))
   :custom
-  (nerd-icons-dired-file-icon-function #'rtw/nerd-icons-icon-for-file)
-  (nerd-icons-dired-dir-icon-function #'rtw/nerd-icons-icon-for-dir)
+  (nerd-icons-dired-file-icon-function #'nerd-icons-icon-for-file)
+  (nerd-icons-dired-dir-icon-function #'nerd-icons-icon-for-dir)
   :hook
   (dired-mode . nerd-icons-dired-mode))
 
